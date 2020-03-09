877db261369d3aa60f03788b27076b7ef8558592
Daniel <dani.andrade@ua.pt>
2020-03-09 17:28:46 +0000

    Interpreter.java modified

--- Guiao2/Ex2/Interpreter.java
+++ Guiao2/Ex2/Interpreter.java
@@ -1,44 +1,39 @@
 import java.util.Stack;
 
-public class Interpreter extends SuffixCalculatorBaseVisitor<String> {
+public class Interpreter extends SuffixCalculatorBaseVisitor<Double> {
 
-    private static char sig = 'a';
-    private static Stack<Integer> stk = new Stack<>();
 
-   @Override public String visitProgram(SuffixCalculatorParser.ProgramContext ctx) {
+
+   @Override public Double visitProgram(SuffixCalculatorParser.ProgramContext ctx) {
       return visitChildren(ctx);
    }
 
-   @Override public String visitStat(SuffixCalculatorParser.StatContext ctx) {
-      return visitChildren(ctx);
+   @Override public Double visitStat(SuffixCalculatorParser.StatContext ctx) {
+     double res = visit(ctx.expr());
+     System.out.println("Resultado: " + res);
+     return res;
    }
 
-   @Override public String visitExprNumber(SuffixCalculatorParser.ExprNumberContext ctx) {
-      stk.push(Integer.parseInt(ctx.Number().getText()));
-      System.out.println("Adicionou");
-      if(sig != 'a' && stk.size()>=2){
-        int result = 0;
-          switch(sig){
-            case '+': result = stk.pop() + stk.pop();
-                      break;
-            case '-': result = stk.pop() - stk.pop();
-                      break;
-            case '*': result = stk.pop() * stk.pop();
-                      break;
-            case '/': result = Math.round(stk.pop()/stk.pop());
-                      break;
-          }
-          stk.push(result);
-          System.out.println(result);
-          sig = 'a';
-      }
-      return visitChildren(ctx);
+   @Override public Double visitExprNumber(SuffixCalculatorParser.ExprNumberContext ctx) {
+
+      return Double.parseDouble(ctx.Number().getText());
    }
 
-   @Override public String visitExprSuffix(SuffixCalculatorParser.ExprSuffixContext ctx) {
-      System.out.println(ctx.getText());
-      sig = ctx.getText().charAt(ctx.getText().length()-1);
-
-      return visitChildren(ctx);
+   @Override public Double visitExprSuffix(SuffixCalculatorParser.ExprSuffixContext ctx) {
+  //   visitChildren(ctx);
+    double v1 = visit(ctx.expr(0));
+    double v2 = visit(ctx.expr(1));
+     double result = 0;
+     switch(ctx.op.getText()){
+       case "+": result = v1 + v2 ;
+                 break;
+       case "-": result = v1 - v2;
+                 break;
+       case "*": result = v1 * v2;
+                 break;
+       case "/": result = v1 / v2;
+                 break;
+     }
+     return result;
    }
 }
